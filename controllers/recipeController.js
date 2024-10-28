2b69607761a9bc341bd167154bff977be6b72286
mparente88 <parente.molly@gmail.com>
2024-10-28 12:09:11 -0700

    complete

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,67 +1,81 @@
-const Recipe = require("../models/recipe");
+const Recipe = require("../models/recipe")
 
 const getAllRecipes = async (req, res) => {
   try {
-    const recipes = await Recipe.find();
-    res.json(recipes);
+    const recipes = await Recipe.find()
+    res.json(recipes)
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const getRecipeById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const recipe = await Recipe.findById(id);
+    const { id } = req.params
+    const recipe = await Recipe.findById(id)
     if (recipe) {
-      return res.json(recipe);
+      return res.json(recipe)
     }
-    return res.status(404).send(`that recipe doesn't exist`);
+    return res.status(404).send(`that recipe doesn't exist`)
   } catch (error) {
     if (error.name === "CastError" && error.kind === "ObjectId") {
-      return res.status(404).send(`That recipe doesn't exist`);
+      return res.status(404).send(`That recipe doesn't exist`)
     }
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
+
+const getRecipeByCuisineName = async (req, res) => {
+  try {
+    const { cuisineName } = req.params
+    const cuisine = await Cuisine.findOne({ name: cuisineName })
+    if (!cuisine) {
+      return res.status(404).send("Cuisine not found")
+    }
+    const recipes = await Recipe.find({ cuisineID: cuisine._id })
+    res.json(recipes)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
 
 const createRecipe = async (req, res) => {
   try {
-    const recipe = await new Recipe(req.body);
-    await recipe.save();
+    const recipe = await new Recipe(req.body)
+    await recipe.save()
     return res.status(201).json({
       recipe,
-    });
+    })
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
-};
+}
 
 const updateRecipe = async (req, res) => {
   try {
-    let { id } = req.params;
-    let recipe = await Recipe.findByIdAndUpdate(id, req.body, { new: true });
+    let { id } = req.params
+    let recipe = await Recipe.findByIdAndUpdate(id, req.body, { new: true })
     if (recipe) {
-      return res.status(200).json(recipe);
+      return res.status(200).json(recipe)
     }
-    throw new Error("recipe not found");
+    throw new Error("recipe not found")
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const deleteRecipe = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleted = await Recipe.findByIdAndDelete(id);
+    const { id } = req.params
+    const deleted = await Recipe.findByIdAndDelete(id)
     if (deleted) {
-      return res.status(200).send("Recipe deleted");
+      return res.status(200).send("Recipe deleted")
     }
-    throw new Error("Recipe not found");
+    throw new Error("Recipe not found")
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 module.exports = {
   getAllRecipes,
@@ -69,4 +83,5 @@ module.exports = {
   createRecipe,
   updateRecipe,
   deleteRecipe,
-};
+  getRecipeByCuisineName,
+}
--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const express = require("express");
-const db = require("./db");
-const cuisineController = require("./controllers/cuisineController");
-const recipeController = require("./controllers/recipeController");
-const directionController = require("./controllers/directionController");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express = require("express")
+const db = require("./db")
+const cuisineController = require("./controllers/cuisineController")
+const recipeController = require("./controllers/recipeController")
+const directionController = require("./controllers/directionController")
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+const PORT = process.env.PORT || 3001
+const app = express()
 
-app.get("/", (req, res) => res.send("This is our landing page!"));
-app.get("/cuisines", cuisineController.getAllCuisines);
-app.get("/cuisines/:id", cuisineController.getCuisineById);
+app.get("/", (req, res) => res.send("This is our landing page!"))
+app.get("/cuisines", cuisineController.getAllCuisines)
+app.get("/cuisines/:id", cuisineController.getCuisineById)
 
-app.get("/recipes", recipeController.getAllRecipes);
-app.get("/recipes/:id", recipeController.getRecipeById);
+app.get("/recipes", recipeController.getAllRecipes)
+app.get("/recipes/:id", recipeController.getRecipeById)
+app.get(
+  "/recipes/cuisine/:cuisineName",
+  recipeController.getRecipesByCuisineName
+)
 
-app.get("/directions", directionController.getAllDirections);
-app.get("/directions/:id", directionController.getDirectionById);
+app.get("/directions", directionController.getAllDirections)
+app.get("/directions/:id", directionController.getDirectionById)
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
+app.use(logger("dev"))
+app.use(bodyParser.json())
 
-app.post("/cuisines", cuisineController.createCuisine);
-app.post("/recipes", recipeController.createRecipe);
-app.post("/directions", directionController.createDirection);
+app.post("/cuisines", cuisineController.createCuisine)
+app.post("/recipes", recipeController.createRecipe)
+app.post("/directions", directionController.createDirection)
 
-app.put(`/cuisines/:id`, cuisineController.updateCuisine);
-app.put(`/recipes/:id`, recipeController.updateRecipe);
-app.put(`/directions/:id`, directionController.updateDirection);
+app.put(`/cuisines/:id`, cuisineController.updateCuisine)
+app.put(`/recipes/:id`, recipeController.updateRecipe)
+app.put(`/directions/:id`, directionController.updateDirection)
 
-app.delete(`/cuisines/:id`, cuisineController.deleteCuisine);
-app.delete(`/recipe/:id`, recipeController.deleteRecipe);
-app.delete(`/direction/:id`, directionController.deleteDirection);
+app.delete(`/cuisines/:id`, cuisineController.deleteCuisine)
+app.delete(`/recipe/:id`, recipeController.deleteRecipe)
+app.delete(`/direction/:id`, directionController.deleteDirection)
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
